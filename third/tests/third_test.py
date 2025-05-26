0fda0d5dd7656c0ecc6dc50945c8c02e0a8bbe09
danielvo <7126118+danielvo@users.noreply.github.com>
2025-05-26 08:26:32 +0000

    test: third test - languages [done_final]

--- third/tests/third_test.py
+++ third/tests/third_test.py
@@ -1,22 +1,6 @@
 from third.third import greeting
 import pytest
-
-def test_greeting_eng():
-    assert greeting("James", "English") == "Hello James!"
-
-def test_greeting_nor():
-    assert greeting("Solveig", "Norwegian") == "Hei Solveig!"
-
-def test_greeting_default():
-    assert greeting("Max", "German") == "I don't speak your language!"
-
-def test_greeting_name_wrong_type():
-    with pytest.raises(TypeError):
-        assert greeting(False, "English") == None
-
-def test_greeting_language_wrong_type():
-    with pytest.raises(TypeError):
-        assert greeting("James", 666) == None
+from contextlib import nullcontext
 
 # write one unit test to cover all test cases of greeting(name, language) in third/third.py
 # also import nullcontext: from contextlib import nullcontext
@@ -27,3 +11,17 @@ def test_greeting_language_wrong_type():
 # remove the old test after you have completed the task
 # add your changes: git add third/tests/third_test.py
 # commit your changes using commit message conventions (https://inpred.github.io/24-03_bioinfo_ws/#19): git commit -m "test: <your commit message>"
+
+@pytest.mark.parametrize(
+    "input_name, input_language, exception, output",
+    [
+        ("James", "English", nullcontext(), "Hello James!"),
+        ("Solveig", "Norwegian", nullcontext(), "Hei Solveig!"),
+        ("Max", "German", nullcontext(), "I don't speak your language!"),
+        (False, "English", pytest.raises(TypeError), None),
+        ("James", 666, pytest.raises(TypeError), None)
+    ]
+)
+def test_greeting(input_name, input_language, exception, output):
+    with exception:
+        assert greeting(input_name, input_language) == output
